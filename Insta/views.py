0312b00004b03f837ff1b4af7d1180ffc4a27008
waynesapphire <wei.xin@husky.neu.edu>
2019-07-24 00:29:56 -0400

    Basic Version-End of Firstday

--- Insta/views.py
+++ Insta/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 # Create your views here.
+from Insta.models import Post
 
 class HelloDjango(TemplateView):
     template_name = 'home.html'
+
+class PostView(ListView):
+    model = Post
+    template_name = 'posts.html'
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
